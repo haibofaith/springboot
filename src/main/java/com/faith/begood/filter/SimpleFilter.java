999ea6d67d624d32dbde16a824dd14d4507a17f7
haibo.xiong <haibo.xiong@ly.com>
2018-12-27 11:41:51 +0800

    filter过滤器

---
+++ src/main/java/com/faith/begood/filter/SimpleFilter.java
@@ -0,0 +1,21 @@
+package com.faith.begood.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author:haibo.xiong
+ * @date:2018/12/27
+ * @description:
+ */
+@Component
+public class SimpleFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Remote Host:"+servletRequest.getRemoteHost());
+        System.out.println("Remote Address:"+servletRequest.getRemoteAddr());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
